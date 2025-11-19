d7368bf041d4c6849bc59e97beacd8f8f52d0025
Hieutritue <124784640+Hieutritue@users.noreply.github.com>
2025-11-19 15:56:21 +0700

    resource storage

---
+++ Assets/Scripts/ResourceSystem/Storage/ResourceStorageDetailUI.cs
@@ -0,0 +1,60 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ResourceSystem.Storage
+{
+    public class ResourceStorageDetailUI : MonoBehaviour
+    {
+        [SerializeField] private Image _iconImage;
+        [SerializeField] private TMP_Text _resourceNameText;
+        [SerializeField] private TMP_Text _maxAmountText;
+        private ResourceSO _resourceSO;
+
+        private int _maxAmount;
+        public int MaxAmount
+        {
+            get => _maxAmount;
+            set
+            {
+                _maxAmount = Mathf.Clamp(value, 0, 999);
+                _maxAmountText.text = _maxAmount.ToString();
+            }
+        }
+
+        public void Setup(ResourceSO resourceSO, int maxAmount)
+        {
+            _resourceSO = resourceSO;
+            _iconImage.sprite = resourceSO.Icon;
+            _resourceNameText.text = resourceSO.ResourceName;
+            // Use the property to ensure clamping and text update
+            MaxAmount = maxAmount;
+        }
+
+        public void IncreaseMaxAmount()
+        {
+            int delta = 1;
+            delta = IsCtrlHeld() ? 10 : delta;
+            delta = IsShiftHeld() ? 100 : delta;
+            MaxAmount += delta;
+        }
+
+        public void DecreaseMaxAmount()
+        {
+            int delta = 1;
+            delta = IsCtrlHeld() ? 10 : delta;
+            delta = IsShiftHeld() ? 100 : delta;
+            MaxAmount -= delta;
+        }
+
+        private bool IsShiftHeld()
+        {
+            return Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+        }
+
+        private bool IsCtrlHeld()
+        {
+            return Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl);
+        }
+    }
+}
---
+++ Assets/Scripts/ResourceSystem/Storage/ResourceStorageManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BuildingSystem;
+using DefaultNamespace;
+using UnityEngine;
+
+namespace ResourceSystem.Storage
+{
+    public class ResourceStorageManager : MonoSingleton<ResourceStorageManager>
+    {
+        [SerializeField] private Transform _supplyParent;
+        [SerializeField] private Transform _researchPointParent;
+        [SerializeField] private Transform _materialParent;
+        
+        [SerializeField] private ResourceStorageDetailUI _resourceStorageDetailUIPrefab;
+        
+        private Dictionary<ResourceType, ResourceStorageDetailUI> _resourceStorageDetailUIs = new ();
+
+        private void Start()
+        {
+            var manager = ResourceManager.Instance;
+
+            foreach (var resourceSo in manager.GetAllResourceSOs())
+            {
+                ResourceStorageDetailUI detail;
+                if (resourceSo.IsSupply)
+                {
+                    detail = Instantiate(_resourceStorageDetailUIPrefab, _supplyParent);
+                }
+                else if (resourceSo.IsResearchPoint)
+                {
+                    detail = Instantiate(_resourceStorageDetailUIPrefab, _researchPointParent);
+                }
+                else
+                    detail = Instantiate(_resourceStorageDetailUIPrefab, _materialParent);
+
+                detail.Setup(resourceSo,999);
+                _resourceStorageDetailUIs[resourceSo.ResourceType] = detail;
+            }
+        }
+
+        public int GetMaxCapacityForResourceType(ResourceType resourceType)
+        {
+            return _resourceStorageDetailUIs.TryGetValue(resourceType, out var detailUI)
+                ? detailUI.MaxAmount
+                : 999;
+        }
+    }
+}
