7aab00cdc2e11f9a3eebdf7b37d8bcdcc2dc7e10
Hieutritue <124784640+Hieutritue@users.noreply.github.com>
2025-11-29 23:18:58 +0700

    fix shield grid data

--- Assets/Scripts/PlacementSystem/GridData.cs
+++ Assets/Scripts/PlacementSystem/GridData.cs
@@ -32,6 +32,17 @@ namespace DefaultNamespace
             return placeableInstance;
         }
 
+        public void RegisterData(Vector3Int gridPosition, Vector2Int size)
+        {
+            var positionToOccupy = CalculatePositions(gridPosition, size);
+            foreach (var position in positionToOccupy.Where(position => !PlacedInstances.TryAdd(position, null)))
+            {
+                throw new Exception($"Occupied position at {position}");
+            }
+            
+            OnGridDataChanged?.Invoke();
+        }
+
         private PlaceableInstance RegisterInstance(List<Vector3Int> positionToOccupy, PlaceableSO placeableSo, GameObject gameObject)
         {
             switch (placeableSo.Type)
