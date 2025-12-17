344fe281920792b869d01b9f6dc60f35e6dedf16
QuocAnh1502 <cungocquocanh1502@gmail.com>
2025-12-17 12:21:15 +0700

    Undone end scene

--- Assets/Scripts/CutScene/TypeWritter.cs
+++ Assets/Scripts/CutScene/TypeWritter.cs
@@ -6,7 +6,8 @@ public class Typewriter : MonoBehaviour
 {
     public TextMeshProUGUI textUI;
     public float typingSpeed = 0.04f;
-
+    [SerializeField] private float warningBlinkSpeed = 0.3f;
+    private Coroutine warningCoroutine;
     private Coroutine typingCoroutine;
 
     public void PlayText(string fullText)
@@ -33,5 +34,41 @@ public class Typewriter : MonoBehaviour
             StopCoroutine(typingCoroutine);
 
         textUI.text = "";
+    }
+    public void PlayWarningSignal()
+    {
+        StopAllTextCoroutines();
+        warningCoroutine = StartCoroutine(WarningLoop());
+    }
+    
+    public void StopWarningSignal()
+    {
+        StopAllTextCoroutines();
+        textUI.text = "";
+    }
+    
+    
+    private IEnumerator WarningLoop()
+    {
+        const string baseText = "Warning";
+
+        int exclamationCount = 1;
+
+        while (true)
+        {
+            textUI.text = baseText + new string('!', exclamationCount);
+            exclamationCount = exclamationCount % 3 + 1;
+
+            yield return new WaitForSeconds(warningBlinkSpeed);
+        }
+    }
+    
+    private void StopAllTextCoroutines()
+    {
+        if (typingCoroutine != null)
+            StopCoroutine(typingCoroutine);
+
+        if (warningCoroutine != null)
+            StopCoroutine(warningCoroutine);
     }
 }
