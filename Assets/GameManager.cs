85648f8aa127e78e1f552a4dbef84c6877ad6335
Hieutritue <124784640+Hieutritue@users.noreply.github.com>
2025-12-18 21:08:14 +0700

    start cutscene

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using DefaultNamespace;
+using MoreMountains.Feedbacks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoSingleton<GameManager>
 {
+    [SerializeField] private MMF_Player _fadeFeedback;
     public void LoadScene(string sceneName)
     {
         StartCoroutine(LoadSceneCoroutine(sceneName, 1));
@@ -19,6 +21,7 @@ public class GameManager : MonoSingleton<GameManager>
 
     public void Win()
     {
-        throw new System.NotImplementedException();
+        _fadeFeedback.PlayFeedbacks();
+        LoadScene("End Game Cutscene");
     }
 }
